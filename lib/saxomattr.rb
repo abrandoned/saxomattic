6ed055268337764e4476fbecaf63a25f63ca2125
Brandon Dewitt <brandonsdewitt@gmail.com>
2013-08-11 13:00:01 -0600

    add support for sax-machine attribute/elements/ancestor/value

--- lib/saxomattr.rb
+++ lib/saxomattr.rb
@@ -4,6 +4,7 @@ require "sax-machine"
 require "active_attr"
 require "active_support/core_ext/hash/slice"
 require "active_support/core_ext/array/extract_options"
+require "active_support/core_ext/string/conversions"
 
 module Saxomattr
   ACTIVE_ATTR_ATTRIBUTES = [
@@ -21,25 +22,30 @@ module Saxomattr
 
   def self.included(klass)
     klass.extend(HookManagementMethods)
+    klass.__send__(:include, ::ActiveAttr::BlockInitialization)
     klass.__send__(:include, ::ActiveAttr::TypecastedAttributes)
     klass.__send__(:include, ::ActiveAttr::QueryAttributes)
-    klass._capture_active_attr_attribute_method(klass)
+    klass.__send__(:include, ::ActiveAttr::Serialization)
+    klass._capture_active_attr_methods(klass)
     klass.__send__(:include, ::SAXMachine)
-    klass._capture_sax_machine_attribute_method(klass)
+    klass._capture_sax_machine_methods(klass)
     klass.extend(ClassMethods)
   end
 
   module HookManagementMethods
-    def _capture_active_attr_attribute_method(klass)
+    def _capture_active_attr_methods(klass)
       class << klass
         alias_method :_active_attr_attribute, :attribute
       end
     end
 
-    def _capture_sax_machine_attribute_method(klass)
+    def _capture_sax_machine_methods(klass)
       class << klass
-        alias_method :_sax_machine_element, :element
+        alias_method :_sax_machine_ancestor, :ancestor
         alias_method :_sax_machine_attribute, :attribute
+        alias_method :_sax_machine_element, :element
+        alias_method :_sax_machine_elements, :elements
+        alias_method :_sax_machine_value, :value
       end
     end
   end
@@ -50,8 +56,19 @@ module Saxomattr
       field = args.first
 
       _active_attr_attribute(field, _active_attr_attributes(options))
-      _sax_machine_element(field, _sax_machine_attributes(options))
-      #_sax_machine_attribute(field, _sax_machine_attributes(options))
+
+      case
+      when options[:ancestor] then
+        _sax_machine_ancestor(field, _sax_machine_attributes(options))
+      when options[:attribute] then
+        _sax_machine_attribute(field, _sax_machine_attributes(options))
+      when options[:elements] then
+        _sax_machine_elements(field, _sax_machine_attributes(options))
+      when options[:value] then
+        _sax_machine_value(field, _sax_machine_attributes(options))
+      else # Default state is an element
+        _sax_machine_element(field, _sax_machine_attributes(options))
+      end
     end
 
     def _active_attr_attributes(options_hash = {})
--- spec/saxomattr_spec.rb
+++ spec/saxomattr_spec.rb
@@ -4,13 +4,21 @@ class SaxTesterSomething
   include Saxomattr
 
   attribute :foo, :type => Integer
+  attribute :iso_8701, :type => Date
+  attribute :date, :type => Date
+  attribute :datetime, :type => DateTime
 end
 
 describe ::Saxomattr do
 
   let(:xml) {
     <<-XML
-    <foo>2</foo>
+    <test>
+      <iso_8701>2013-01-13</iso_8701>
+      <datetime>#{DateTime.current}</datetime>
+      <date>#{Date.today}</date>
+      <foo>2</foo>
+    </test>
     XML
   }
 
